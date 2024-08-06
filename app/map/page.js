cd4351629f7d29a991ff995f538f70c6c0679145
Abdulrahman7010 <abdulrahmanolayinka827@gmail.com>
2024-08-06 15:35:58 +0100

    postgress

--- app/map/page.js
+++ app/map/page.js
@@ -4,7 +4,8 @@ import React, { useRef, useEffect, useState } from 'react';
 import 'mapbox-gl/dist/mapbox-gl.css';
 import mapboxgl from 'mapbox-gl'; 
 import Header from '../siginUp/nav';
-
+import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
+import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
 
 mapboxgl.accessToken = 'pk.eyJ1IjoibXV6YW1pbDIwNiIsImEiOiJjbGN5eXh2cW0wc2lnM290ZzJsZnNlbmxsIn0.o2Obvl7E_nQefSN34XsFmw';
 
@@ -13,7 +14,7 @@ export default function App() {
   const map = useRef(null);
   const [lng, setLng] = useState(4.474076);
   const [lat, setLat] = useState(8.70274);
-  const [zoom, setZoom] = useState(15);
+  const [zoom, setZoom] = useState(12);
   const [data, setData] = useState(null); 
   
 
@@ -22,16 +23,17 @@ export default function App() {
 
     map.current = new mapboxgl.Map({
       container: mapContainer.current,
-      style: 'mapbox://styles/mapbox/streets-v12',
+      style: 'mapbox://styles/mapbox/satellite-streets-v12',
       center: [lng, lat],
       zoom: zoom
     });
 
     const fetchGeoJSON = async () => {
-      const response = await fetch('https://eu-west-2.aws.data.mongodb-api.com/app/application-0-zzfnd/endpoint/data');
-      const data = await response.json();
-      setData(data);
-      console.log(data[0])
+      // const response = await fetch('http://localhost:8080/geoserver/cadastral/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=cadastral%3Apolygon%20table&maxFeatures=50&outputFormat=application%2Fjson');
+      
+      // const data = await response.json();
+      // setData(data);
+      // console.log(data[0])
       
     
 
@@ -43,9 +45,8 @@ export default function App() {
       // Create an empty source to hold all features
       const source = map.current.addSource('allFeaturesSource', {
         type: 'geojson',
-        data: data[0]
+        data: 'http://localhost:8080/geoserver/cadastral/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=cadastral%3Acombined_view&maxFeatures=50&outputFormat=application%2Fjson',
       });
-
       // Loop through GeoJSON features and add them to the source
       if (data && data.features) {
         data.features.forEach(feature => {
@@ -91,6 +92,19 @@ export default function App() {
         }
       });
 
+      map.current.addLayer({
+        id: 'poi-labels',
+        type: 'symbol',
+        source: 'allFeaturesSource',
+        layout: {
+          'text-field': ['get', 'pillar_num'],
+          'text-variable-anchor': ['top', 'bottom', 'left', 'right'],
+          'text-radial-offset': 0.5,
+          'text-justify': 'auto',
+          'icon-image': ['get', 'icon']
+        }
+      });
+
 
 // Change the cursor to a pointer when
 // the mouse is over the states layer.
@@ -107,14 +121,18 @@ map.current.on('mouseenter', 'polygonsLayers', () => {
   map.current.on('click', 'polygonsLayer', (e) => {
     new mapboxgl.Popup()
     .setLngLat(e.lngLat)
-    .setHTML(`<div> NAME: ${e.features[0].properties.NAME}</div> 
-     <div> LOCATION :  <p> ${e.features[0].properties.PLAN_LOCAT} </p> </div>   
-     <div> LOCAL GOV : <p>${e.features[0].properties.PLAN_LGA} </p> </div> 
-     <div> PLAN AREA : <p>${e.features[0].properties.PLAN_AREA} </p> </div> 
-     <div> PLAN ORIGIN : <p>${e.features[0].properties.PLAN_ORIGI} </p> </div> 
-     <div> PLAN NUMBER :  <p>${e.features[0].properties.PLAN_NUMBE} </p> </div> 
-     <div> SURVEYOR :   <p> ${e.features[0].properties.SURVEYOR} </p> </div> 
-    `)
+    .setHTML(`
+    <div class="bg-gray-100 p-4 rounded-lg shadow-md">
+      <div class="text-lg font-bold mb-2">NAME: ${e.features[0].properties.plan_name}</div>
+      <div class="mb-2">LOCATION: <p class="text-gray-700">${e.features[0].properties.plan_locat}</p></div>
+      <div class="mb-2">LOCAL GOV: <p class="text-gray-700">${e.features[0].properties.plan_lga}</p></div>
+      <div class="mb-2">PLAN AREA: <p class="text-gray-700">${e.features[0].properties.plan_area}</p></div>
+      <div class="mb-2">PLAN ORIGIN: <p class="text-gray-700">${e.features[0].properties.plan_origi}</p></div>
+      <div class="mb-2">PLAN NUMBER: <p class="text-gray-700">${e.features[0].properties.plan_numbe}</p></div>
+      <div class="mb-2">SURVEYOR: <p class="text-gray-700">${e.features[0].properties.surveyor}</p></div>
+    </div>
+  `)
+  
     .addTo(map.current);
     });
 
@@ -125,7 +143,23 @@ map.current.on('mouseenter', 'polygonsLayers', () => {
     }
     fetchGeoJSON();
 
-    map.current.addControl(new mapboxgl.NavigationControl(), "top-right");
+    map.current.addControl(new mapboxgl.NavigationControl(), "top-left");
+
+
+    map.current.addControl(
+      new MapboxGeocoder({
+        accessToken: mapboxgl.accessToken,
+        bbox: [2.05, 7.7, 5.4, 9.4],
+        countries: 'NG', // Limit search results to Nigeria
+        mapboxgl: mapboxgl,
+        position: 'top-left' 
+      })
+    );
+
+    
+  
+
+
   }, []);
 
   return (
