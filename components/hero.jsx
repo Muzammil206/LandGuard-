5abf6a8a2302294fa45cd3e639cd133e0af8aa4a
Muzammil206 <ismailmuzammil206@gmail.com>
2024-09-27 12:16:06 +0100

    change data

--- components/hero.jsx
+++ components/hero.jsx
@@ -42,11 +42,11 @@ const Hero = () => {
 
           {/* Buttons */}
           <div className="mt-7 grid gap-3 w-full sm:inline-flex px-6 lg:px-0">
-            <Link className="py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-gradient-to-tl from-blue-600 to-violet-600 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none" href="/siginUp">
+            <Link className="py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-gradient-to-tl from-blue-600 to-violet-600 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none" href="/map">
               view map 
               <svg className="flex-shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="m9 18 6-6-6-6"/></svg>
             </Link>
-            <Link className="py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none" href="/login">
+            <Link className="py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none" href="/about">
               About us
             </Link>
           </div>
